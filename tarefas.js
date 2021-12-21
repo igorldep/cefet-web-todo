75d997e9849a2795d8599828c4764ee592937849
Igor De Paula <igordepaula@192.168.1.5>
2021-12-21 19:11:51 -0300

    ex1

--- tarefas.js
+++ tarefas.js
@@ -4,3 +4,30 @@ let tarefas = [
     { nome: "Escutar música boa", categoria: "lazer", realizada: true }
 ];
 
+const listaTarefas = document.querySelector('ul#lista-tarefas');
+
+//1
+function InsereTarefaNaPagina (tarefa) {
+    const elementos = document.createElement('li');
+    
+    elementos.classList.add('item-tarefa');
+    elementos.classList.add(`categoria-${tarefa.categoria}`);
+
+    elementos.addEventListener('click', () => {
+        elementos.classList.toggle('marcado');
+        tarefa.realizada = !tarefa.realizada;
+    })
+
+    if(tarefa.realizada)
+        elementos.classList.add('marcado');    
+    
+    const textoElemento = document.createElement('p');
+    textoElemento.innerHTML = tarefa.nome;
+
+    elementos.appendChild(textoElemento);
+    listaTarefas.appendChild(elementos);
+}
+
+
+listaTarefas.innerHTML = '';
+tarefas.forEach(insereTarefaNaPagina);
